bf949f341c3dc1627e9797736966fcf39f6d934a
gayeonni <gayeoneez@gmail.com>
2024-07-03 12:35:36 +0900

    Update AppController.java

--- src/main/java/com/playdata/cpuboundapp/controller/AppController.java
+++ src/main/java/com/playdata/cpuboundapp/controller/AppController.java
@@ -21,7 +21,7 @@ public class AppController {
 
     @RequestMapping("/hello")
     public String hello() {
-        return "Hi";
+        return "Hi -update-";
     }
 
     private String getMD5Digest(String input) throws NoSuchAlgorithmException {
